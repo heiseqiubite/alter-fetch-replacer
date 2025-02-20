df9cd16dcf38fb1d4c7c8242e576b609d650dafa
heiseqiubite <40165253+heiseqiubite@users.noreply.github.com>
2025-02-20 14:03:55 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,134 +1,35 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+// 完全修改掉 main.ts
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, requestUrl } from 'obsidian';
+import fetch from "node-fetch";
+type FetchFunction = typeof window.fetch;
 
-// Remember to rename these classes and interfaces!
+// 这个 as unknown 啥意思? 
+// 目前已熟练掌握了 "类型体操" 四个字的拼写, 其余都不懂
+const node_fetch: FetchFunction = fetch as unknown as FetchFunction;
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+export default class NoProblemsFetch extends Plugin {
+    originalFetch: any;
+    async onload () {
+        this.originalFetch = window.fetch;
+		console.log(`预备使用 node-fetch 替代 window.fetch`);
+		
+        window.fetch = node_fetch;
+		
+		const fetch_funcs = [this.originalFetch, node_fetch, window.fetch];
+		const current_fetch_desc = fetch_funcs[2] === fetch_funcs[0] ? '原有的fetch':'插件提供的fetch';
+		console.log(fetch_funcs);
+		console.log(`当前fetch为: ${current_fetch_desc} \n以上三个函数分别是 [原始fetch, 新node-fetch, 目前采用fetch]`);
+		new Notice(`使用 node-fetch 替代 window.fetch\n现在是: ${current_fetch_desc}`);
+    }
+    onunload () {
+		console.log(`预备撤销对 window.fetch 的更改`);
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+		window.fetch = this.originalFetch;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+		const fetch_funcs = [this.originalFetch, node_fetch, window.fetch];
+		const current_fetch_desc = fetch_funcs[2] === fetch_funcs[0] ? '原有的fetch':'插件提供的fetch';
+		console.log(fetch_funcs);
+		console.log(`当前fetch为: ${current_fetch_desc} \n以上三个函数分别是 [原始fetch, 新node-fetch, 目前采用fetch]`);
+		new Notice(`撤销对 window.fetch 的更改\n现在是: ${current_fetch_desc}`);
+    }
 }
